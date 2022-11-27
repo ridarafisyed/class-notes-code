9cbd28ce4cb3b99247689f33915266aa5bdc17c5
Rida R Syed <ridarafisyed@yahoo.com>
2022-11-27 11:04:27 +0500

    upload class project

---
+++ class02/src/index.ts
@@ -0,0 +1,10 @@
+/** @format */
+
+//union:
+// we can declare with multiple types
+// we can use all the functions related to types we are using at the time
+let variable: string | number = "hello world";
+// variable= true
+
+// there are two type of method to narrow down the type
+const philosopher = "Hello";
